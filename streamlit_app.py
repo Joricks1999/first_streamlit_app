d0544bdd439bd27e0d194f17bed6c0c649166350
Joricks1999 <114674444+Joricks1999@users.noreply.github.com>
2022-09-29 20:05:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
-streamlit.title('My parents new Healthy Diner')
-streamlit.title('Breakfast menu')
-streamlit.title('Heyy Listen to this music')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
